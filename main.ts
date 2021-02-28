b5874bbca1d13195bb28d6655026577e8b15828d
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2021-02-28 19:51:08 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Merhaba")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Selam")
+})
+basic.clearScreen()
+basic.forever(function () {
+	
+})
